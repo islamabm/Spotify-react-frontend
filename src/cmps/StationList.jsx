6bdf2bad8fcdaa0380ecd2befc8a5bcddebabd76
Bar-Barina <itsbarbarina@gmail.com>
2023-06-20 20:57:39 +0300

    added input in header

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -2,11 +2,14 @@ import { memo } from "react";
 import { StationPreview } from "./StationPreview";
 function _StationList({ stations, onRemoveStation }) {
     return (
+        <>
+            {/* <h2>Sing-along</h2> */}
         <section className="station-list">
             {stations.map(station =>
                 <StationPreview key={station._id} station={station} onRemoveStation={onRemoveStation} />
             )}
         </section>
+        </>
     )
 }
 
