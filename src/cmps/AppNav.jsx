7583445f1b2464b8f250f77a64513853cbe7b2f7
Bar-Barina <itsbarbarina@gmail.com>
2023-06-20 13:51:03 +0300

    added styling for appNav

--- src/cmps/AppNav.jsx
+++ src/cmps/AppNav.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React from "react";
+import { getSpotifySvg } from "../services/SVG.service";
 
 export function AppNav() {
-  return <div className="app-nav">AppNav</div>
+  return (
+    <section className="app-nav">
+      <div className="nav-home-div">
+      <span
+              dangerouslySetInnerHTML={{
+                __html: getSpotifySvg('homeIcon'),
+              }}
+            ></span>
+        <span>Home</span>
+      </div>
+      <div className="nav-search-div">
+      <span
+              dangerouslySetInnerHTML={{
+                __html: getSpotifySvg('searchIcon'),
+              }}
+            ></span>
+        <span>Search</span>
+      </div>
+    </section>
+  );
 }
