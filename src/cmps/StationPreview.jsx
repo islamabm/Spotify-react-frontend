d25417c4533039643e13bf6d2af6d445544a9940
Itay komemi <itaykomemi8@gmail.com>
2023-06-20 15:55:08 +0300

    station cards pixel perfect

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
 
-export function StationPreview({ station, onRemoveStation }) {
-  const stationStyle = {
-    backgroundImage: `url(https://robohash.org/${station._id})`,
-  }
+export function StationPreview({ station }) {
   return (
-    <article className="station-preview">
-      <Link to={`/station/${station._id}`} className="info">
-        <img className='station-img' src={station.imgUrl} alt="" />
-        <h2>{station.name}</h2>
-      </Link>
-    </article>
+    <Link to={`/station/${station._id}`} className="info">
+      <article className="station-preview">
+        <div className="station-img">
+          <img src={station.imgUrl} alt="" />
+        </div>
+        <div className="station-info">
+          <h3>{station.name}</h3>
+          <p>{station.description}</p>
+        </div>
+      </article>
+    </Link>
   )
 }
