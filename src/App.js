26f81e63d7f3b6c4ba7ba218a2d57c86f94aa1e5
islam abo mokh <alo.jalal.55@gmail.com>
2023-06-20 10:47:40 +0300

    bug with the render of app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
-import { Route, HashRouter as Router, Routes, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import './assets/scss/global.scss'
 import { AppHeader } from './cmps/AppHeader'
 import { RobotIndex } from './views/RobotIndex'
-import { About } from './views/About';
-import { RobotDetails } from './views/RobotDetails';
-import { RobotEdit } from './views/RobotEdit';
-
-const Team = () => {
-    return (
-        <ul>
-            <li>Moshe Girit</li>
-            <li>Dan Gonzales</li>
-            <li>Shimon Dicaprio</li>
-        </ul>
-    )
-}
-
-const Vision = () => {
-    return (
-        <ol>
-            <li>Save the world with our robots</li>
-            <li>Take over the world with our robots</li>
-        </ol>
-    )
-}
+import { RobotDetails } from './views/RobotDetails'
+import { RobotEdit } from './views/RobotEdit'
 
 function App() {
-    return (
-        <Router>
-            <section className="main-app">
-                <AppHeader />
+  return (
+    <Router>
+      <section className="main-app">
+        <AppHeader />
 
-                <main className="container">
-                    <Routes>
-                        <Route path="/" element={<RobotIndex isDark={true} />} />
-                        <Route path="/robot/edit/:id?" element={<RobotEdit />} />
-                        <Route path="/robot/:id" element={<RobotDetails />} />
-                        <Route path="/about" element={<About />} >
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<RobotIndex />} />
+            <Route path="/robot/edit/:id?" element={<RobotEdit />} />
+            <Route path="/robot/:id" element={<RobotDetails />} />
+            {/* <Route path="/about" element={<About />} >
                             <Route path="/about/team" element={<Team />} />
                             <Route path="/about/vision" element={<Vision />} />
-                        </Route>
-                    </Routes>
-                </main>
-
-                <footer>
-                    <section className="container">
-                        robotRights 2022 &copy;
-                    </section>
-                </footer>
-
-            </section>
-        </Router>
-    )
+                        </Route> */}
+          </Routes>
+        </main>
+      </section>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/views/RobotIndex.jsx
+++ src/views/RobotIndex.jsx
@@ -1,67 +1,31 @@
-import { Component, useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import { RobotList } from '../cmps/RobotList'
-import { RobotFilter } from '../cmps/RobotFilter'
 import { Link } from 'react-router-dom'
-import { NiceButton } from '../cmps/NiceButton'
-import { connect, useDispatch, useSelector } from 'react-redux'
-import { loadRobots, removeRobot, setFilterBy } from '../store/actions/robot.actions'
-import { spendBalance } from '../store/actions/user.actions'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadRobots, removeRobot } from '../store/actions/robot.actions'
 
-export function RobotIndex(props) {
-    const robots = useSelector((storeState) => storeState.robotModule.robots)
-    const filterBy = useSelector((storeState) => storeState.robotModule.filterBy)
-    const dispatch = useDispatch()
+export function RobotIndex() {
+  const robots = useSelector((storeState) => storeState.robotModule.robots)
+  const dispatch = useDispatch()
 
-    const [isPopo, setIsPopo] = useState(true)
+  useEffect(() => {
+    dispatch(loadRobots())
+  }, [])
 
-    useEffect(() => {
-        dispatch(loadRobots())
-    }, [])
-
-    const onRemoveRobot = useCallback(async (robotId) => {
-        try {
-            dispatch(removeRobot(robotId))
-        } catch (error) {
-            console.log('error:', error)
-        }
-    }, [])
-
-    const onChangeFilter = (filterBy) => {
-        dispatch(setFilterBy(filterBy))
-        dispatch(loadRobots())
+  const onRemoveRobot = useCallback(async (robotId) => {
+    try {
+      dispatch(removeRobot(robotId))
+    } catch (error) {
+      console.log('error:', error)
     }
+  }, [])
 
-    const onSpendBalance = () => {
-        dispatch(spendBalance(5))
-    }
-    
-    const bigNum = useMemo(() => {
-        let sum = 0
-        for (let i = 0; i <= 10 ** 8 * 3; i++) {
-            sum += i
-        }
-        return sum * (robots?.length || 1)
-    }, [robots?.length])
+  if (!robots) return <div>Loading...</div>
 
-
-
-    if (!robots) return <div>Loading...</div>
-    const NiceText = () => 'Nice Text'
-    const Icon = () => '🏆'
-    console.log('ROBOT INDEX');
-    return (
-        <section className='robot-index'>
-            {bigNum}
-            <RobotFilter filterBy={filterBy} onChangeFilter={onChangeFilter} />
-            <Link to="/robot/edit">Add Robot</Link>
-            <RobotList robots={robots} onRemoveRobot={onRemoveRobot} />
-            <NiceButton Icon={Icon} className='nice-button' onClick={() => console.log('Clicked!')} >
-                <NiceText />
-            </NiceButton>
-            <NiceButton onClick={onSpendBalance} className='nice-button' Icon={() => '💰'}>Spend Balance</NiceButton>
-            <NiceButton onClick={() => setIsPopo((prevIsPopo) => !prevIsPopo)} className='nice-button' Icon={() => '💰'}>
-                {isPopo ? 'YES POPO' : 'NO POPO'}
-            </NiceButton>
-        </section>
-    )
+  return (
+    <section className="robot-index">
+      {/* <Link to="/robot/edit">Add Robot</Link> */}
+      <RobotList robots={robots} onRemoveRobot={onRemoveRobot} />
+    </section>
+  )
 }
