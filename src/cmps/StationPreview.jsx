33223e57ea186683f5066a8a9d09d5bbd6afb3d8
islam abo mokh <alo.jalal.55@gmail.com>
2023-06-20 13:18:18 +0300

    colors

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export function StationPreview({ station, onRemoveStation }) {
-  // const stationStyle = { backgroundImage: `url(https://robohash.org/${station._id})` }
+  const stationStyle = {
+    backgroundImage: `url(https://robohash.org/${station._id})`,
+  }
   return (
     <article className="station-preview">
-      {/* <Link to={`/station/${station._id}`} className="info"> */}
-      {/* <h2>{station.model}</h2> */}
-      {/* <h4>{station.type}</h4> */}
-      {/* </Link> */}
-      {/* <section className="actions"> */}
-      {/* <button onClick={() => onRemoveStation(station._id)} >X</button> */}
-      {/* <Link to={`/station/edit/${station._id}`}>Edit</Link> */}
-      {/* </section> */}
+      <Link to={`/station/${station._id}`} className="info">
+        <h2>{station.model}</h2>
+        <h4>{station.type}</h4>
+      </Link>
+      <section className="actions">
+        <button onClick={() => onRemoveStation(station._id)}>X</button>
+        <Link to={`/station/edit/${station._id}`}>Edit</Link>
+      </section>
     </article>
   )
 }
