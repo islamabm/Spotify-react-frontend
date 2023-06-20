56b9f507e5ea2b2c0982d54a448a62f665e27545
Bar-Barina <itsbarbarina@gmail.com>
2023-06-20 22:38:14 +0300

    added libray btns

--- src/cmps/UserLibrary.jsx
+++ src/cmps/UserLibrary.jsx
@@ -1,5 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getSpotifySvg } from "../services/SVG.service";
 
 export function UserLibrary() {
-  return <div className="user-library">UserLibrary</div>
+  return (
+    <section className="user-library">
+      <div className="flex align-center library-header">
+        <div className="flex align-center your-library pointer">
+      <span
+        dangerouslySetInnerHTML={{
+          __html: getSpotifySvg("libraryIconActive"),
+        }}
+      ></span>
+      <span>Your Library</span>
+      </div>
+      <span className="plus-icon flex align-center justify-center pointer"
+        dangerouslySetInnerHTML={{
+          __html: getSpotifySvg("plus"),
+        }}
+      ></span>
+      </div>
+      <div className="your-library-btns flex align-center">
+        <button className="library-btn">Playlists</button>
+        <button className="library-btn">Artists</button>
+        <button className="library-btn">Albums</button>
+      </div>
+    </section>
+  );
 }
