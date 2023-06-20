9a5386c69c82e52b49fc7e0da055f26f32a76bdd
Bar-Barina <itsbarbarina@gmail.com>
2023-06-20 14:35:15 +0300

    added routing to appNav

--- src/cmps/AppNav.jsx
+++ src/cmps/AppNav.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import { getSpotifySvg } from "../services/SVG.service";
 
 export function AppNav() {
+  const location = useLocation();
+
+  const getSvgIcon = (iconName) => {
+    if (iconName === "homeIcon" && location.pathname === "/") {
+      return getSpotifySvg("homeIconActive");
+    }
+    if (iconName === "searchIcon" && location.pathname === "/search") {
+      return getSpotifySvg("searchIconActive");
+    }
+    return getSpotifySvg(iconName);
+  };
+  
   return (
     <section className="app-nav">
-      <div className="nav-home-div">
-      <span
-              dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('homeIcon'),
-              }}
-            ></span>
+      <Link exact to="/" className="shared-nav-item">
+        <span dangerouslySetInnerHTML={{ __html: getSvgIcon("homeIcon") }}></span>
         <span>Home</span>
-      </div>
-      <div className="nav-search-div">
-      <span
-              dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('searchIcon'),
-              }}
-            ></span>
+      </Link>
+      <Link to="/search" className="shared-nav-item">
+        <span dangerouslySetInnerHTML={{ __html: getSvgIcon("searchIcon") }}></span>
         <span>Search</span>
-      </div>
+      </Link>
     </section>
   );
 }
