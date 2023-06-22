9b0b41fced63198d0fe66f2908b30c223c40d9d8
Itay komemi <itaykomemi8@gmail.com>
2023-06-22 12:34:57 +0300

    station details styling and layout

--- src/views/StationDetails.jsx
+++ src/views/StationDetails.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
-import { stationService } from '../services/station.service'
-import { useParams } from 'react-router-dom'
-import { getSpotifySvg } from '../services/SVG.service'
-import { useDispatch, useSelector } from 'react-redux'
-import { setCurrStation } from '../store/actions/station.actions'
-import { setCurrSong } from '../store/actions/song.actions'
+import { useEffect, useState } from "react"
+import { stationService } from "../services/station.service"
+import { useParams } from "react-router-dom"
+import { getSpotifySvg } from "../services/SVG.service"
+import { useDispatch, useSelector } from "react-redux"
+import { setCurrStation } from "../store/actions/station.actions"
+import { setCurrSong } from "../store/actions/song.actions"
 export function StationDetails(props) {
   // const [station, setStation] = useState(null)
   const params = useParams()
@@ -13,7 +13,7 @@ export function StationDetails(props) {
     (storeState) => storeState.stationModule.currStation
   )
   const song = useSelector((storeState) => storeState.songModule.currSong)
-  console.log('song', song)
+  console.log("song", song)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -34,20 +34,31 @@ export function StationDetails(props) {
     dispatch(setCurrSong(params.id, songId))
   }
 
+  function stationNameClass() {
+    const words = station.name.split(" ").length
+    if (words <= 3) {
+      return "short-station-name"
+    } else if (words <= 5) {
+      return "long-station-name"
+    } else {
+      return "huge-station-name"
+    }
+  }
+
   function formatDate(dateString) {
     const months = [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
     ]
 
     const date = new Date(dateString)
@@ -70,21 +81,21 @@ export function StationDetails(props) {
         />
         <div className="station-info">
           <span className="playlist-word">Playlist</span>
-          <h1 className="station-name">{station.name}</h1>
+          <h1 className={stationNameClass()}>{station.name}</h1>
           <p className="station-description">{station.description}</p>
           <span className="logo">Spotify </span>
           <span class="dot">• </span>
           <span class="songs-count"> {station.songs.length} songs </span>
         </div>
       </div>
-      <div className="station-songs-header">
-        <span>#</span>
-        <span>Title</span>
-        <span>Album</span>
-        <span>Date added</span>
-        <span>#</span>
-      </div>
       <div className="station-songs">
+        <div className="station-songs-header">
+          <span className="flex align-center justify-center">#</span>
+          <span className="title flex align-center">Title</span>
+          <span className="flex align-center">Album</span>
+          <span className="flex align-center">Date added</span>
+          <span className="flex align-center justify-center">#</span>
+        </div>
         {station.songs.map((song, idx) => (
           <div key={idx} className="song">
             <span className="song-idx flex align-center justify-center">
@@ -108,12 +119,12 @@ export function StationDetails(props) {
             <div className="duration-container flex">
               <input className="hidden" type="checkbox" />
               <div className="duration">
-                {song.duration ? song.duration : '1:00'}
+                {song.duration ? song.duration : "1:00"}
               </div>
               <span
                 className="hidden dots"
                 dangerouslySetInnerHTML={{
-                  __html: getSpotifySvg('dots'),
+                  __html: getSpotifySvg("dots"),
                 }}
               ></span>
             </div>
