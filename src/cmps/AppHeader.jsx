b03f6d598bc3e5cde4adc29c531fd369eb02470f
Itay komemi <itaykomemi8@gmail.com>
2023-06-25 19:46:11 +0300

    header styling adjusments

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,9 +1,9 @@
-import { getSpotifySvg } from '../services/SVG.service'
-import { useLocation, Link } from 'react-router-dom'
-import { UserModal } from './UserModal'
-import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { eventBus } from '../services/event-bus.service'
+import { getSpotifySvg } from "../services/SVG.service"
+import { useLocation, Link } from "react-router-dom"
+import { UserModal } from "./UserModal"
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import { eventBus } from "../services/event-bus.service"
 export function AppHeader() {
   const [showModal, setShowModal] = useState(false)
   const [currScrollPos, setScrollPos] = useState(0)
@@ -15,16 +15,16 @@ export function AppHeader() {
   const gradient = useSelector(
     (storeState) => storeState.stationModule.currStationGradientColor
   )
-  console.log('gradient', gradient)
+  console.log("gradient", gradient)
   const location = useLocation()
   // const [headerOpacity, setHeaderOpacity] = useState(0)
   const [headers, setHeaders] = useState({
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   })
 
   function updateHeaderOpacity(scrollPos) {
-    console.log('hi')
-    console.log('scrollPos', scrollPos)
+    console.log("hi")
+    console.log("scrollPos", scrollPos)
     setScrollPos(scrollPos)
 
     const maxScroll = 50
@@ -37,22 +37,22 @@ export function AppHeader() {
       //       .slice(1, 4)
       //       .join(', ')
       //   :
-      '0, 0, 0'
+      "0, 0, 0"
 
     const newHeaders = {
       backgroundColor: `rgba(${dominantColor}, ${opacity})`,
     }
 
-    console.log('after creating the styles ')
+    console.log("after creating the styles ")
 
     setHeaders(newHeaders)
 
-    console.log('after update the state')
+    console.log("after update the state")
   }
 
   useEffect(() => {
     const onScroll = (scrollPos) => updateHeaderOpacity(scrollPos)
-    const unlisten = eventBus.on('stationDetailsScroll', onScroll)
+    const unlisten = eventBus.on("stationDetailsScroll", onScroll)
 
     return () => {
       unlisten()
@@ -77,19 +77,19 @@ export function AppHeader() {
           <div className="black-circle">
             <span
               dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('leftArrowIcon'),
+                __html: getSpotifySvg("leftArrowIcon"),
               }}
             ></span>
           </div>
           <div className="black-circle">
             <span
               dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('rightArrowIcon'),
+                __html: getSpotifySvg("rightArrowIcon"),
               }}
             ></span>
           </div>
         </section>
-        {location.pathname === '/search' && (
+        {location.pathname === "/search" && (
           <div className="flex align-center justify-center">
             <input placeholder="What do you want to listen to?" />
           </div>
@@ -100,20 +100,21 @@ export function AppHeader() {
             <p>{station.name}</p>
           </div>
         ) : (
-          ''
+          ""
         )}
       </section>
 
-      <div className="user-actions">
+      <div className="user-actions flex justify-center align-center">
         <button className="sign-up pointer">Sign up</button>
         <Link to="/login">
-          <button className="login pointer">Log in</button>
+          <button className="login pointer flex">Log in</button>
         </Link>
-        <img
-          onClick={onShowModal}
-          src="https://i.scdn.co/image/ab6761610000e5eb601fb0059594d52f3f7939a9"
-          className="pointer"
-        />
+
+        <span className="user-icon pointer flex justify-center align-center"
+          dangerouslySetInnerHTML={{
+            __html: getSpotifySvg("userIcon"),
+          }}
+        ></span>
       </div>
       {showModal && <UserModal onClose={onCloseModal} />}
     </header>
