d22a824525d6b356f62910999738c2d539aecf7b
Itay komemi <itaykomemi8@gmail.com>
2023-06-25 14:41:19 +0300

    station header details component

---
+++ src/views/StationHeaderDetails.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+export default function StationHeaderDetails(props) {
+  const bgStyle = props.bgStyle
+  const station = props.station
+
+  function stationNameClass() {
+    const words = station.name.split(" ").length
+    if (words <= 3) {
+      return "short-station-name"
+    } else if (words <= 5) {
+      return "long-station-name"
+    } else {
+      return "huge-station-name"
+    }
+  }
+  return (
+    <div className="station-header-content" style={bgStyle}>
+      <img
+        className="station-main-img"
+        src={station.imgUrl}
+        alt="station main img"
+      />
+      <div className="station-info">
+        <span className="playlist-word">Playlist</span>
+        <h1 className={stationNameClass()}>{station.name}</h1>
+        <p className="station-description">{station.description}</p>
+
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/7/74/Spotify_App_Logo.svg"
+          className="spotify-logo"
+        />
+        <span className="logo">Spotify </span>
+        <span className="dot">• </span>
+        <span className="songs-count"> {station.songs.length} songs </span>
+      </div>
+    </div>
+  )
+}
