a945c9c71c8172abea1aac262435e25e3a189623
Bar-Barina <itsbarbarina@gmail.com>
2023-06-27 12:52:44 +0300

    fixed drag and drop

--- src/cmps/StationSongList.jsx
+++ src/cmps/StationSongList.jsx
@@ -1,143 +1,161 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { getSpotifySvg } from '../services/SVG.service'
-import { setCurrSong, setCurrSongIndex } from '../store/actions/song.actions'
-import { useParams } from 'react-router-dom'
-import { SongOptionsModal } from './SongOptionsModal'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getSpotifySvg } from "../services/SVG.service";
+import { setCurrSong, setCurrSongIndex } from "../store/actions/song.actions";
+import { useParams } from "react-router-dom";
+import { SongOptionsModal } from "./SongOptionsModal";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { updateStation } from "../store/actions/station.actions";
 
 export default function StationSongList(props) {
-  const station = props.station
-  const dispatch = useDispatch()
-  const params = useParams()
-  const [hoveredSongIdx] = useState(null)
-  const [showModal, setShowOptionsModal] = useState(false)
-  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 })
-  const [songs, setSongs] = useState(station.songs)
+  const station = props.station;
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [hoveredSongIdx] = useState(null);
+  const [showModal, setShowOptionsModal] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+  const [songs, setSongs] = useState(station.songs);
   function onSongClicked(songId) {
-    dispatch(setCurrSong(params.id, songId))
-    dispatch(setCurrSongIndex(params.id, songId))
+    dispatch(setCurrSong(params.id, songId));
+    dispatch(setCurrSongIndex(params.id, songId));
   }
 
   function showSongOptionsModal(e, songId) {
-    dispatch(setCurrSong(params.id, songId))
+    dispatch(setCurrSong(params.id, songId));
 
-    const rect = e.target.getBoundingClientRect()
+    const rect = e.target.getBoundingClientRect();
     setModalPosition({
       top: rect.top + window.scrollY,
       left: rect.left + window.scrollX,
-    })
-    console.log('showModal', showModal)
-    setShowOptionsModal(true)
+    });
+    console.log("showModal", showModal);
+    setShowOptionsModal(true);
   }
 
   function formatDate(dateString) {
     const months = [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
-    ]
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
 
-    const date = new Date(dateString)
-    const monthIndex = date.getMonth()
-    const day = date.getDate()
-    const year = date.getFullYear()
+    const date = new Date(dateString);
+    const monthIndex = date.getMonth();
+    const day = date.getDate();
+    const year = date.getFullYear();
 
-    const formattedDate = `${months[monthIndex]} ${day}, ${year}`
-    return formattedDate
+    const formattedDate = `${months[monthIndex]} ${day}, ${year}`;
+    return formattedDate;
   }
 
   function handleDragEnd(result) {
     if (!result.destination) {
-      return // The item was dropped outside of a droppable area
+      return;
     }
 
-    const { source, destination } = result
+    const { source, destination } = result;
 
-    // Create a new array to avoid mutating the state directly
-    const updatedSongs = Array.from(songs)
+    const updatedSongs = Array.from(songs);
 
-    // Reorder the songs based on the drag and drop result
-    const [movedSong] = updatedSongs.splice(source.index, 1)
-    updatedSongs.splice(destination.index, 0, movedSong)
+    const [movedSong] = updatedSongs.splice(source.index, 1);
+    updatedSongs.splice(destination.index, 0, movedSong);
 
-    // Update the state with the new song order
-    setSongs(updatedSongs)
+    setSongs(updatedSongs);
 
-    // Dispatch the updateStation action with the new song order
-    dispatch(updateStation(params.id, updatedSongs))
+    dispatch(updateStation(params.id, updatedSongs));
   }
   return (
-    <div>
-      {station.songs?.map((song, idx) => (
-        <div key={idx} className="song">
-          <span
-            className={`song-idx flex align-center justify-center ${
-              hoveredSongIdx === idx ? 'hovered' : ''
-            }`}
-          >
-            {idx + 1}
-          </span>
-          <span
-            className={` small-play-btn flex align-center justify-center ${
-              hoveredSongIdx === idx ? 'hovered' : ''
-            }`}
-            dangerouslySetInnerHTML={{
-              __html: getSpotifySvg('smallPlayButton'),
-            }}
-          ></span>
-          <div className="song-details-container">
-            <div className="img-container flex align-center justify-center">
-              <img
-                onClick={() => onSongClicked(song._id)}
-                className="song-img"
-                src={song.imgUrl}
-                alt="song img"
-              />
-            </div>
-            <div className="name-and-artist flex justify-center">
-              <span className="song-name">{song.title}</span>
-              <span className="song-artist">{song.artist}</span>
-            </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="songList">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {songs.map((song, idx) => (
+              // SONG
+              <Draggable key={song._id} draggableId={song._id} index={idx}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="song"
+                  >
+                    <span
+                      className={`song-idx flex align-center justify-center ${
+                        hoveredSongIdx === idx ? "hovered" : ""
+                      }`}
+                    >
+                      {idx + 1}
+                    </span>
+                    <span
+                      className={` small-play-btn flex align-center justify-center ${
+                        hoveredSongIdx === idx ? "hovered" : ""
+                      }`}
+                      dangerouslySetInnerHTML={{
+                        __html: getSpotifySvg("smallPlayButton"),
+                      }}
+                    ></span>
+                    <div className="song-details-container">
+                      <div className="img-container flex align-center justify-center">
+                        <img
+                          onClick={() => onSongClicked(song._id)}
+                          className="song-img"
+                          src={song.imgUrl}
+                          alt="song img"
+                        />
+                      </div>
+                      <div className="name-and-artist flex justify-center">
+                        <span className="song-name">{song.title}</span>
+                        <span className="song-artist">{song.artist}</span>
+                      </div>
+                    </div>
+                    <div className="album-name flex align-center">
+                      {song.album}
+                    </div>
+                    <div className="added-at flex align-center">
+                      {formatDate(song.addedAt)}
+                    </div>
+                    <div className="duration-container flex">
+                      <span
+                        className="hidden dots"
+                        dangerouslySetInnerHTML={{
+                          __html: getSpotifySvg("emptyHeartIcon"),
+                        }}
+                      ></span>
+                      <div className="duration">
+                        {song.duration ? song.duration : "1:00"}
+                      </div>
+                      <span
+                        onClick={(e) => showSongOptionsModal(e, song._id)}
+                        className="hidden dots"
+                        dangerouslySetInnerHTML={{
+                          __html: getSpotifySvg("dots"),
+                        }}
+                      ></span>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+              // SONG
+            ))}
+            {provided.placeholder}
           </div>
-          <div className="album-name flex align-center">{song.album}</div>
-          <div className="added-at flex align-center">
-            {formatDate(song.addedAt)}
-          </div>
-          <div className="duration-container flex">
-            <span
-              className="hidden dots"
-              dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('emptyHeartIcon'),
-              }}
-            ></span>
-            <div className="duration">
-              {song.duration ? song.duration : '1:00'}
-            </div>
-            <span
-              onClick={(e) => showSongOptionsModal(e, song._id)}
-              className="hidden dots"
-              dangerouslySetInnerHTML={{
-                __html: getSpotifySvg('dots'),
-              }}
-            ></span>
-          </div>
-        </div>
-      ))}
+        )}
+      </Droppable>
       {showModal && (
         <SongOptionsModal
           position={modalPosition}
           // closeOptionsModal={() => setShowOptionsModal(false)}
         />
       )}
-    </div>
-  )
+    </DragDropContext>
+  );
 }
--- src/store/reducers/station.reducer.js
+++ src/store/reducers/station.reducer.js
@@ -1,29 +1,29 @@
-export const SET_STATIONS = 'SET_STATIONS'
-export const SET_SEARCH_STATIONS = 'SET_SEARCH_STATIONS'
-export const ADD_STATION = 'ADD_STATION'
-export const REMOVE_STATION = 'REMOVE_STATION'
-export const UPDATE_STATION = 'UPDATE_STATION'
-export const SET_FILTER_BY = 'SET_FILTER_BY'
-export const SET_CURR_STATION = 'SET_CURR_STATION'
-export const SET_CURR_GRADIENT = 'SET_CURR_GRADIENT'
-export const LOAD_USER_STATIONS = 'LOAD_USER_STATIONS'
-export const ADD_SONG_TO_STATION = 'ADD_SONG_TO_STATION'
+export const SET_STATIONS = "SET_STATIONS";
+export const SET_SEARCH_STATIONS = "SET_SEARCH_STATIONS";
+export const ADD_STATION = "ADD_STATION";
+export const REMOVE_STATION = "REMOVE_STATION";
+export const UPDATE_STATION = "UPDATE_STATION";
+export const SET_FILTER_BY = "SET_FILTER_BY";
+export const SET_CURR_STATION = "SET_CURR_STATION";
+export const SET_CURR_GRADIENT = "SET_CURR_GRADIENT";
+export const LOAD_USER_STATIONS = "LOAD_USER_STATIONS";
+export const ADD_SONG_TO_STATION = "ADD_SONG_TO_STATION";
 const INITIAL_STATE = {
-  currStationImg: '',
+  currStationImg: "",
   stations: [],
   searchStations: null,
   filterBy: {
-    model: '',
-    type: '',
-    minBatteryStatus: '',
-    maxBatteryStatus: '',
+    model: "",
+    type: "",
+    minBatteryStatus: "",
+    maxBatteryStatus: "",
   },
   currStation: null,
   currStationId: null,
-  currStationGradientColor: 'black',
+  currStationGradientColor: "black",
   userStations: [],
-  currStationType: 'system',
-}
+  currStationType: "system",
+};
 
 export function stationReducer(state = INITIAL_STATE, action = {}) {
   switch (action.type) {
@@ -31,70 +31,63 @@ export function stationReducer(state = INITIAL_STATE, action = {}) {
       return {
         ...state,
         stations: action.stations,
-      }
+      };
     case LOAD_USER_STATIONS:
       return {
         ...state,
         userStations: action.stations,
-      }
+      };
     case SET_CURR_GRADIENT:
       return {
         ...state,
         currStationGradientColor: { ...action.bgStyle },
-      }
+      };
     case SET_CURR_STATION:
       return {
         ...state,
         currStation: action.station,
         currStationId: action.station._id,
         currStationImg: action.station.imgUrl,
-      }
+      };
     case ADD_SONG_TO_STATION:
       return {
         ...state,
         stations: state.stations.map((station) =>
           station._id === action.station._id ? action.station : station
         ),
-      }
+      };
     case SET_SEARCH_STATIONS:
       return {
         ...state,
         searchStations: action.stations,
-      }
+      };
     case ADD_STATION:
       return {
         ...state,
         stations: [...state.stations, action.station],
         userStations: [...state?.userStations, action.station],
-      }
+      };
     case REMOVE_STATION:
       return {
         ...state,
         stations: state.stations.filter(
           (station) => station._id !== action.stationId
         ),
-      }
-    case UPDATE_STATION:
-      return {
-        ...state,
-        stations: state.stations.map((station) =>
-          station._id === action.station._id ? action.station : station
-        ),
-      }
+      };
     case SET_FILTER_BY:
       return {
         ...state,
         filterBy: { ...action.filterBy },
-      }
+      };
     case UPDATE_STATION:
       return {
         ...state,
         stations: state.stations.map((station) =>
           station._id === action.station._id ? action.station : station
         ),
-      }
+      };
 
     default:
-      return state
+      return state;
   }
 }
