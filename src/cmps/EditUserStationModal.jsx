cdc8993fbe90183880e34601c358c094aab9994a
Bar-Barina <itsbarbarina@gmail.com>
2023-06-28 21:38:09 +0300

    fixed some modal sass

--- src/cmps/EditUserStationModal.jsx
+++ src/cmps/EditUserStationModal.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {getSpotifySvg} from '../services/SVG.service'
+import { getSpotifySvg } from "../services/SVG.service";
 import { useState } from "react";
 
 export function EditUserStationModal() {
   const [isHovered, setIsHovered] = useState(false);
+  const [focusedInput, setFocusedInput] = useState(null);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -12,27 +13,58 @@ export function EditUserStationModal() {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+
+  const handleInputFocus = (inputIndex) => {
+    setFocusedInput(inputIndex);
+  };
+
+  const handleInputBlur = () => {
+    setFocusedInput(null);
+  };
+
   return (
     <section className="modal-container">
       <div className="modal-header flex align-center">
         <h2>Edit details</h2>
-        <span>X</span>
+        <span
+            className="x flex align-center justify-center pointer"
+            dangerouslySetInnerHTML={{
+              __html: getSpotifySvg("x"),
+            }}
+          ></span>
       </div>
       <div className="edit-actions">
-        <div class="default-image-div"
+        <div
+          className="default-image-div"
           onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
-        <span
-          className="music-note"
-          dangerouslySetInnerHTML={{
-            __html: isHovered ? getSpotifySvg("editImgIcon") : getSpotifySvg("userStationImg"),
-          }}
-        ></span>
+          onMouseLeave={handleMouseLeave}
+        >
+          <span
+            className="music-note"
+            dangerouslySetInnerHTML={{
+              __html: isHovered
+                ? getSpotifySvg("editImgIcon")
+                : getSpotifySvg("userStationImg"),
+            }}
+          ></span>
         </div>
 
         <div className="flex column inputs-container">
-          <input type="text" placeholder="Add a name"/>
-          <input className="bigger-input" type="text" placeholder="Add an optional description"/>
+          <input
+            type="text"
+            placeholder="Add a name"
+            onFocus={() => handleInputFocus(1)}
+            onBlur={handleInputBlur}
+          />
+          {focusedInput === 1 && <span className="name-span">Name</span>}
+          <input
+            className="bigger-input"
+            type="text"
+            placeholder="Add an optional description"
+            onFocus={() => handleInputFocus(2)}
+            onBlur={handleInputBlur}
+          />
+          {focusedInput === 2 && <span className="descp-span">Description</span>}
         </div>
       </div>
       <button>Save</button>
