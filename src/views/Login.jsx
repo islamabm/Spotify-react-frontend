5ae56c90bfac627a98a3387734569248215a6582
Bar-Barina <itsbarbarina@gmail.com>
2023-07-01 11:38:30 +0300

    added login scss

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,39 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { doLogin } from '../store/actions/user.actions'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { doLogin } from "../store/actions/user.actions";
+import { useNavigate,Link } from "react-router-dom";
+import {getSpotifySvg} from "../services/SVG.service"
 export function Login() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const users = useSelector((storeState) => storeState.userModule.users)
-  const navigate = useNavigate()
-  const [loginCred, setLoginCred] = useState({ username: '', password: '' })
+  const users = useSelector((storeState) => storeState.userModule.users);
+  const navigate = useNavigate();
+  const [loginCred, setLoginCred] = useState({ username: "", password: "" });
 
   function handleLogin(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (!loginCred.username || !loginCred.password) {
-      alert('Please enter username/password')
-      return
+      alert("Please enter username/password");
+      return;
     }
-    dispatch(doLogin(loginCred))
-    navigate(`/`)
+    dispatch(doLogin(loginCred));
+    navigate(`/`);
   }
 
   const handleInputChange = (e) => {
     setLoginCred({
       ...loginCred,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
   return (
     <section className="spotify-login-page">
+      <div className="login-header"> <span
+                className="flex"
+                dangerouslySetInnerHTML={{
+                  __html: getSpotifySvg('LoginHeaderLogo'),
+                }}
+              ></span></div>
+              <div className="login-main-container flex column align-center">
+                 <h2>Log in to Spotify</h2>
+                 <div className="continueW flex column">
+                  <button><span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"/></span>Continue With Google</button>
+                  <button><span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"/></span>Continue With Facebook</button>
+                  <button><span><img src="https://1000logos.net/wp-content/uploads/2016/10/apple-emblem.jpg"/></span>Continue With Apple</button>
+                 </div>
+                 <div className="empty-line"></div>
       <form className="spotify-login-form" onSubmit={handleLogin}>
-        <div className="spotify-form-group">
-          <label className="login-form-labels" htmlFor="username">
-            Username
-          </label>
-          <select
+        <div className="spotify-username flex column">
+          <span className="login-form-labels">
+           Email or username
+          </span>
+          <input type="text name=username" placeholder="Email or username"/>
+          {/* <select
             className="login-form-inputs"
             id="username"
             name="username"
@@ -46,12 +62,12 @@ export function Login() {
                 {user.fullname}
               </option>
             ))}
-          </select>
+          </select> */}
         </div>
-        <div className="spotify-form-group">
-          <label className="login-form-labels" htmlFor="password">
+        <div className="spotify-password flex column">
+          <span className="login-form-labels">
             Password
-          </label>
+          </span>
           <input
             className="login-form-inputs"
             type="password"
@@ -62,8 +78,14 @@ export function Login() {
             onChange={handleInputChange}
           />
         </div>
-        <button>Login</button>
+        <button className="login-btn">Log in</button>
       </form>
+      <div className="empty-line"></div>
+              <div className="go-to-signup flex justify-center">
+                <span>Don't have an account?</span>
+                <Link to="/signup">Sign up for Spotify</Link>
+              </div>
+              </div>
     </section>
-  )
+  );
 }
