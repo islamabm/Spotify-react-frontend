5cde3e684ec1966fa3b4b5adab1f318e865fa939
Bar-Barina <itsbarbarina@gmail.com>
2023-07-03 17:27:31 +0300

    added latest scss, not final

--- src/cmps/LatestStations/LatestStationsPreview.jsx
+++ src/cmps/LatestStations/LatestStationsPreview.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function LatestStationsPreview({ station }) {
   return (
     station && (
-        <article className="latest-station-article">{station.name}</article>
-
+        <article className="latest-stations-article flex align-center">
+          <img src={station.imgUrl} />
+          <span>{station.name}</span>
+          </article>
     )
   );
 }
