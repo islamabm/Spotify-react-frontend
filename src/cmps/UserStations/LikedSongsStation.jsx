a3cf8ca05506187d23bf3c7f02c54e4053893956
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-04 01:16:41 +0300

    fix create new station bug

--- src/cmps/UserStations/LikedSongsStation.jsx
+++ src/cmps/UserStations/LikedSongsStation.jsx
@@ -5,9 +5,11 @@ import { useDispatch } from 'react-redux'
 import emptyImg from '../../assets/imgs/empty-img.png'
 import { useSelector } from 'react-redux'
 import { setCurrStation } from '../../store/actions/station.actions'
+
 export default function LikedSongsStation() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+
   function goToUserStationDetails() {
     const stationId = '64a31f7a56a7902b69ccef0a'
     dispatch(setCurrStation(stationId))
@@ -16,24 +18,29 @@ export default function LikedSongsStation() {
 
   const user = useSelector((storeState) => storeState.userModule.loggedInUser)
 
-  return (
-    <section className="user-station-preview" onClick={goToUserStationDetails}>
-      <div className="image-svg-container">
-        <img
-          src="https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"
-          alt="station-img"
-        />
-      </div>
-      <div className="user-station-details">
-        <div className="user-station-name">
-          <p>Liked Songs</p>
+  if (user && user.LikedSongs && user.LikedSongs.length !== 0) {
+    return (
+      <section
+        className="user-station-preview"
+        onClick={goToUserStationDetails}
+      >
+        <div className="image-svg-container">
+          <img
+            src="https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"
+            alt="station-img"
+          />
         </div>
-        <div className="user-details">
-          <span>Playlist</span>
-          <span>•</span>
-          <span>{user?.username}</span>
+        <div className="user-station-details">
+          <div className="user-station-name">
+            <p>Liked Songs</p>
+          </div>
+          <div className="user-details">
+            <span>Playlist</span>
+            <span>•</span>
+            <span>{user?.username}</span>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
