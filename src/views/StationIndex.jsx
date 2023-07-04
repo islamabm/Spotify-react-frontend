41017c1336c4e60947b6ba3970b1c63edf257558
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-04 23:07:38 +0300

    bug in details fixed

--- src/views/StationIndex.jsx
+++ src/views/StationIndex.jsx
@@ -1,30 +1,30 @@
-import { useEffect } from "react";
-import { StationList } from "../cmps/StationList";
-import { useDispatch, useSelector } from "react-redux";
-import { loadStations } from "../store/actions/station.actions";
-import { LatestStationsIndex } from "../cmps/LatestStations/LatestStationsIndex";
+import { useEffect } from 'react'
+import { StationList } from '../cmps/StationList'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadStations } from '../store/actions/station.actions'
+import { LatestStationsIndex } from '../cmps/LatestStations/LatestStationsIndex'
 
 export function StationIndex() {
   const stations = useSelector(
     (storeState) => storeState.stationModule.stations
-  );
+  )
 
   const filterBy = useSelector(
     (storeState) => storeState.stationModule.categoryBy
-  );
+  )
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(loadStations(filterBy));
-  }, [filterBy]);
+    dispatch(loadStations(filterBy))
+  }, [filterBy])
 
-  if (!stations) return <div className="loader"></div>;
+  if (!stations) return <div className="loader"></div>
 
   return (
     <section className="station-index">
-      <LatestStationsIndex />
+      {/* <LatestStationsIndex /> */}
       <StationList stations={stations} />
     </section>
-  );
+  )
 }
