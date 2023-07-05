908a01a830348b1e15d4f2ea408ee218d2b11720
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-05 16:49:17 +0300

    bug in remove song from liked songs in the back

--- src/cmps/UserStations/UserStationsIndex.jsx
+++ src/cmps/UserStations/UserStationsIndex.jsx
@@ -3,20 +3,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import UserStationsList from './UserStationsList'
 import { loadUserStations } from '../../store/actions/station.actions'
 
-export function UserStationsIndex({filterUserStations}) {
+export function UserStationsIndex({ filterUserStations }) {
   const userStations = useSelector(
     (storeState) => storeState.stationModule.userStations
   )
+
   // const user = useSelector((storeState) => storeState.userModule.loggedInUser)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(loadUserStations())
-  }, [])
+  }, [userStations])
 
   return (
     <section className="user-stations-index">
-      <UserStationsList userStations={userStations} filterUserStations={filterUserStations}/>
+      <UserStationsList
+        userStations={userStations}
+        filterUserStations={filterUserStations}
+      />
     </section>
   )
 }
