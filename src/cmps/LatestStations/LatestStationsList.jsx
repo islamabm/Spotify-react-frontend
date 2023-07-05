b4aa309abff28242ca5c89ac9e72d79c1ce68590
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-05 09:18:58 +0300

    bug in key for LatestStationsList fixed

--- src/cmps/LatestStations/LatestStationsList.jsx
+++ src/cmps/LatestStations/LatestStationsList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import LatestStationsPreview from './LatestStationsPreview'
 
-export function LatestStationsList({latestStations}) {
+export function LatestStationsList({ latestStations }) {
   return (
-    ( latestStations &&
-    <section className='latest-stations-container'>
-      {latestStations?.map((station, idx) => (
-        <LatestStationsPreview key={station.id} station={station} />
-      ))}
-    </section>
-  ))
+    latestStations && (
+      <section className="latest-stations-container">
+        {latestStations?.map((station, idx) => (
+          <LatestStationsPreview key={station._id} station={station} />
+        ))}
+      </section>
+    )
+  )
 }
