eb7cd1ec0e45aeab4e8b434d0f13c7665aeed744
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-08 21:06:38 +0300

    bug in close search option in user station

--- src/cmps/SearchSongs/SearchSongsIndex.jsx
+++ src/cmps/SearchSongs/SearchSongsIndex.jsx
@@ -3,10 +3,10 @@ import { getSpotifySvg } from '../../services/SVG.service'
 import { stationService } from '../../services/station.service'
 import SearchSongsList from './SearchSongsList'
 
-export default function SearchSongsIndex({ stationId }) {
+export function SearchSongsIndex({ stationId }) {
   const [searchText, setSearchText] = useState('')
   const [songList, setSongList] = useState([])
-
+  const [isOpen, setIsOpen] = useState(true)
   useEffect(() => {
     const timerId = setTimeout(async () => {
       const list = await stationService.getVideos(searchText)
@@ -22,32 +22,42 @@ export default function SearchSongsIndex({ stationId }) {
     setSearchText(e.target.value)
   }
 
+  function closeSerach() {
+    setIsOpen(false)
+  }
+
   return (
     <div className="search-songs-container">
-      <h2>Let's find something for your playlist</h2>
-      <button className="search-songs-x">
-        <span
-          className="search-icon"
-          dangerouslySetInnerHTML={{
-            __html: getSpotifySvg('x'),
-          }}
-        ></span>
-      </button>
-      <div className="input-container">
-        <span
-          className="search-icon"
-          dangerouslySetInnerHTML={{
-            __html: getSpotifySvg('searchIcon'),
-          }}
-        ></span>
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search for songs"
-          value={searchText}
-          onChange={handleInputChange}
-        />
-      </div>
+      {isOpen ? (
+        <>
+          <h2>Let's find something for your playlist</h2>
+          <button className="search-songs-x pointer" onClick={closeSerach}>
+            <span
+              className="search-icon pointer"
+              dangerouslySetInnerHTML={{
+                __html: getSpotifySvg('x'),
+              }}
+            ></span>
+          </button>
+          <div className="input-container">
+            <span
+              className="search-icon"
+              dangerouslySetInnerHTML={{
+                __html: getSpotifySvg('searchIcon'),
+              }}
+            ></span>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search for songs"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+          </div>
+        </>
+      ) : (
+        <h1>shlock</h1>
+      )}
       <div className="station-songs search">
         {searchText.length > 0 && (
           <SearchSongsList list={songList} stationId={stationId} />
