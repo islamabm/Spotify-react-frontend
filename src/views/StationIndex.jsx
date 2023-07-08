a12c24c418f1b3d1bd21fa0a7817586117b223c3
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-08 19:23:36 +0300

    bug in loader fixed

--- src/views/StationIndex.jsx
+++ src/views/StationIndex.jsx
@@ -48,7 +48,7 @@ export function StationIndex() {
     dispatch(loadStations())
   }, [])
 
-  if (!stations) return <div className="loader"></div>
+  if (!stations) return <div className="loader">loading</div>
 
   return (
     <section className="station-index" ref={stationIndexRef}>
