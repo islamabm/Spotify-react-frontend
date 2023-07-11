4981e06c34c79a6c39b7974450964fea765026df
Itay komemi <itaykomemi8@gmail.com>
2023-07-11 14:55:55 +0300

    search songs list

--- src/cmps/SearchSongs/SearchSongsPreview.jsx
+++ src/cmps/SearchSongs/SearchSongsPreview.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { addSongToStation } from '../../store/actions/station.actions'
-import { utilService } from '../../services/util.service'
-import { setVideoId } from '../../store/actions/song.actions'
+import React from "react"
+import { useDispatch } from "react-redux"
+import { addSongToStation } from "../../store/actions/station.actions"
+import { utilService } from "../../services/util.service"
+import { setVideoId } from "../../store/actions/song.actions"
 
 export function SearchSongsPreview({ song, stationId }) {
   const dispatch = useDispatch()
-
+  console.log("song", song)
   function playSong() {
     dispatch(setVideoId(song.videoId, song))
   }
@@ -18,7 +18,10 @@ export function SearchSongsPreview({ song, stationId }) {
     <article className="recommended-song flex align-center">
       <div className="img-and-title flex align-center" onClick={playSong}>
         <img src={song.imgUrl} className="song-img" />
-        <span>{song?.title}</span>
+        <div className="name-and-artist-container flex justify-center">
+          <span className="song-name pointer">{song?.title}</span>
+          <span className="song-artist">{song?.artist}</span>
+        </div>
       </div>
       {stationId && (
         <button onClick={handleAddSong} className="pointer">
